5f628ca303dabbccff830feeced3c522d5108b95
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-23 12:01:10 -0700

    As done as it's going to get
    Like I went over with Casey, I still had weird behavior in the
    controller and didn't have access to the Best Buy API response in the
    controller when I wasn't using byebug or pry...

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,9 @@ class SearchController < ApplicationController
   def index
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&pageSize=16&show=longName,city,distance,phone,storeType&apiKey=#{ENV['BEST_BUY_KEY']}")
     raw_stores_data = JSON.parse(response.body, symbolize_names: true)
-    @raw_stores = raw_stores_data[:stores]
-    @stores = @raw_stores.map do |raw_store|
+    raw_stores = raw_stores_data[:stores]
+    @stores_count = raw_stores.count
+    @stores = raw_stores[0..9].map do |raw_store|
       Store.new(raw_store)
     end
   end
