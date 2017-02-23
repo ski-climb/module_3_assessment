b314274c58e4e4e69ae0d88a9c4a81a653431424
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-23 09:51:14 -0700

    Building a PORO to pull the request logic out of the controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,27 @@
 class SearchController < ApplicationController
 
   def index
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,city,distance,phone,storeType&apiKey=#{ENV['BEST_BUY_KEY']}")
+    raw_stores = JSON.parse(response.body, symbolize_names: true)[:stores]
+    @stores = raw_stores.map do |raw_store|
+      Store.new(raw_store)
+    end
+    binding.pry
+  end
+end
 
+class Store
+  attr_reader :long_name,
+              :city,
+              :distance,
+              :phone,
+              :store_type 
+
+  def initialize(data = {})
+    data[:longName]  = @long_name
+    data[:city]      = @city
+    data[:distance]  = @distance
+    data[:phone]     = @phone
+    data[:storeType] = @store_type
   end
 end
